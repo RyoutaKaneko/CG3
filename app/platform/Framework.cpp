55010834dd9b3eb30a0677e6cb59009407048894
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2023-06-07 10:08:01 +0900

    出力フォルダを外に

---
+++ app/platform/Framework.cpp
@@ -0,0 +1,89 @@
+#include "RLFramework.h"
+
+void RLFramework::Run()
+{
+	// ゲームの初期化
+	Initialize();
+
+	while (true) // ゲームループ 
+	{
+		// 毎フレーム更新
+		Update();
+
+		// 終了リクエストが来たらループを抜ける
+		if (IsEndRequest()) {
+			// ゲームループを抜ける
+			break;
+		}
+
+		// 描画
+		Draw();
+	}
+
+	// ゲームの終了
+	Finalize();
+}
+
+void RLFramework::Initialize()
+{
+	// WindowsAPIの初期化
+	winApp = WinApp::GetInstance();
+	winApp->Initialize();
+
+	// DirectXの初期化
+	dXCommon = DirectXCommon::GetInstance();
+	dXCommon->Initialize(winApp);
+
+	// 入力の初期化
+	input = Input::GetInstance();
+	input->Initialize(winApp);
+
+	// ImGuiの初期化
+	imGuiManager = new ImGuiManager();
+	imGuiManager->Initialize(dXCommon, winApp);
+
+	// ポストエフェクトの初期化
+	postEffect_ = PostEffect::GetInstance();
+	postEffect_->Initialize();
+
+	// 3Dオブジェクト静的初期化
+	Object3d::StaticInitialize(dXCommon->GetDevice(), WinApp::window_width, WinApp::window_height);
+
+	//レール描画静的初期化
+	Line::StaticInitialize(dXCommon->GetDevice());
+
+	// パーティクル静的初期化
+	ParticleManager::StaticInitialize(dXCommon->GetDevice());
+
+	// ビュープロジェクションの初期化
+	ViewProjection::StaticInitialize(dXCommon->GetDevice());
+
+}
+
+void RLFramework::Update()
+{
+	// Windowsのメッセージ処理
+	if (winApp->ProcessMessage()) {
+		// ゲームループを抜ける
+		endRequest_ = true;
+	}
+
+	// 入力の更新
+	input->Update();
+}
+
+void RLFramework::Finalize()
+{
+	// imguiの終了処理
+	imGuiManager->Finalize();
+	// imguiの解放
+	delete imGuiManager;
+	imGuiManager = nullptr;
+
+	// WindowsAPIの終了処理
+	winApp->Finalize();
+	
+	// DirectX解放
+	dXCommon->fpsFixedFinalize();
+	
+}
---
+++ app/scene/BaseScene.cpp
@@ -0,0 +1,67 @@
+#include "RideLight.h"
+
+void RideLight::Initialize()
+{
+	// 基底クラスの初期化
+	RLFramework::Initialize();
+
+	dXCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンの生成と初期化
+	scene_ = new GamePlayScene();
+	scene_->Initialize(spriteCommon);
+}
+
+void RideLight::Update()
+{
+	// 基底クラスの更新処理
+	RLFramework::Update();
+
+	// ImGui受付開始
+	imGuiManager->Begin();
+
+	// ゲームシーンの更新
+	scene_->Update(spriteCommon);
+
+	// ImGui受付終了
+	imGuiManager->End();
+}
+
+void RideLight::Draw()
+{
+#pragma region ゲームシーン描画
+	// レンダーテクスチャの前処理
+	postEffect_->PreDrawScene(dXCommon->GetCommandList());
+
+	//=== ゲームシーン描画 ===//
+	scene_->Draw(spriteCommon);
+
+	// レンダーテクスチャの後処理
+	postEffect_->PostDrawScene(dXCommon->GetCommandList());
+#pragma endregion
+
+#pragma region 描画
+	// 描画前処理
+	dXCommon->PreDraw();
+	
+	//=== ポストエフェクトの描画 ===//
+	postEffect_->Draw(dXCommon->GetCommandList());
+
+	//=== ImGui描画 ===//
+	imGuiManager->Draw(dXCommon);
+
+	// 描画後処理
+	dXCommon->PostDraw();
+#pragma endregion
+}
+
+void RideLight::Finalize()
+{
+	// ゲームシーンの終了処理
+	scene_->Finalize();
+	// ゲームシーンの解放
+	delete scene_;
+
+	// 基底クラスの終了処理
+	RLFramework::Finalize();
+}
